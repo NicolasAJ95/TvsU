f2307ee11b4ea3fd7dbf3a980fe6f1cfb5356ba8
damogi <daniel.montes@upb.edu.co>
2016-10-03 12:01:08 -0500

    Escena Montes y trabajo con los colliders para pasajeros

---
+++ Assets/Scripts/GestorPasajeros.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestorPasajeros : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] puntosPasajero;
+
+	private int pasajero;
+	private bool montado;
+
+	// Use this for initialization
+	void Start () {
+		montado = true;
+		pasajero = Random.Range (0, puntosPasajero.Length);
+		puntosPasajero [pasajero].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//montado=recojido ();
+		if (!montado) {
+			pasajero = Random.Range (0, puntosPasajero.Length);
+			puntosPasajero [pasajero].SetActive (true);
+		}
+	}
+
+	/*bool recojido ()
+	{
+		montado = puntosPasajero [pasajero].
+		return montado;
+	}*/
+}
---
+++ Assets/Scripts/Recoger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recoger : MonoBehaviour {
+
+	public bool aBordo;
+
+	// Use this for initialization
+	void Start () {
+		aBordo = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		Debug.Log ("Algo entró");
+		if (c.gameObject.CompareTag("Player")) 
+		{
+			Debug.Log ("Pasajero a bordo");
+			aBordo = true;
+		}
+	}
+}
