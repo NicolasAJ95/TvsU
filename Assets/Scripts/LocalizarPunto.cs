6355fafc0f9b387c99f74d5885042e1d64e537e1
Daniel Montes <dmontesgil@gmail.com>
2016-10-19 14:08:27 -0500

    Integracion de los scripts en la escena del juego

---
+++ Assets/Scripts/LocalizarPunto.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalizarPunto : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] objetivo;
+
+	void Update () {
+        for (int i = 0; i < objetivo.Length; i++)
+        {
+            if (objetivo[i].gameObject.activeInHierarchy==true)
+            {
+                transform.LookAt(objetivo[i].transform);
+            }
+        }
+	}
+}
