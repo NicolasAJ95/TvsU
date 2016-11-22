69f5325e16eb196f99c1ddec05a5bd6d28828a06
NicolasAJ95 <nicolas.ajaramillo@live.com>
2016-11-21 21:22:04 -0500

    Interfaces ultimas y singleton audio

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+
+    static MusicManager instance = null;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Objeto duplicado eliminado.");
+        }
+        else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+}
